8104412ab200de78047b9c702df8bbe0339e2b42
xMartyczex <82147616+xMartyczex@users.noreply.github.com>
2024-01-25 19:41:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,18 +5,6 @@ function moveButton(button) {
         const x = distance * Math.cos(angle * Math.PI / 180);
         const y = distance * Math.sin(angle * Math.PI / 180);
 
-        // Get the viewport dimensions
-        const viewportWidth = 414;
-        const viewportHeight = 896;
-
-        // Calculate the maximum allowable coordinates
-        const maxX = viewportWidth - button.clientWidth;
-        const maxY = viewportHeight - button.clientHeight;
-
-        // Ensure the button stays within the viewport bounds
-        const constrainedX = Math.max(0, Math.min(maxX, x));
-        const constrainedY = Math.max(0, Math.min(maxY, y));
-
-        button.style.transform = `translate(${constrainedX}px, ${constrainedY}px)`;
+        button.style.transform = `translate(${x}px, ${y}px)`;
     }
 }
